4ff724fa7fbffb40250e570836bd9649222a5971
ace2921 <austinmwelwa44@gmail>
2024-10-01 11:58:03 +0200

    i added the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Navbar.css'
 
+
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <img src="/nu_metro_logo_orange.png" alt="" />
     </div>
   )
 }

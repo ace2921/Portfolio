443891800870469a21c6db58645303b9a22fe00a
ace2921 <austinmwelwa44@gmail>
2024-10-02 16:09:34 +0200

    i am reaching the movie movie part

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Book from './Components/Book/Book';
 import Hero from './Components/Hero/Hero';
 import Icon from './Components/Iconbar/Icon';
 import Navbar from './Components/Navbar/Navbar';
+import Movies from './Components/Movies/Movies';
 
 function App() {
   return (
-    <><Icon /><Navbar /><Hero/><Book/></>
+    <><Icon /><Navbar /><Hero/><Book/><Movies/></>
   );
 }
 

1e4fe4ca3e352b9adb4c0279a03b982d84bbd387
ace2921 <austinmwelwa44@gmail>
2024-10-07 14:15:12 +0200

    Pushing the codenow"

--- src/Components/Movies/Movies.jsx
+++ src/Components/Movies/Movies.jsx
@@ -27,17 +27,28 @@ const Movies = () => {
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-        <SwiperSlide>Slide 10</SwiperSlide>
-        <SwiperSlide>Slide 11</SwiperSlide>
+        <SwiperSlide><img src="5289-1-4-3-1721308188.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6097-1-2-3-1717594468.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6098-1-3-3-1722967951.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6106-1-2-3-1723018318.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6381-1-3-3-1727425309.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6485-1-5-3-1723803980.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6537-1-2-3-1716371101.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6542-1-2-3-1715763299.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6600-1-1-3-1719589140.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6605-1-2-3-1722605713.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6610-1-2-3-1721041041.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6612-1-1-3-1724321033.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6614-1-1-3-1724837095.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6615-1-2-3-1726236464.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6616-1-2-3-1726483631.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6618-1-2-3-1725893580.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6621-1-1-3-1727708358.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6642-1-1-3-1725221375.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6643-1-1-3-1725220008.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6644-1-1-3-1724681504.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6662-1-1-3-1726234131.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="6665-1-1-3-1726483529.jpg" alt="" /></SwiperSlide>
       </Swiper>
     </>
   );

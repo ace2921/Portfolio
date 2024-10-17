b1b0aa6a90e5221189608ebee140240fd5434735
ace2921 <austinmwelwa44@gmail>
2024-10-17 14:02:37 +0200

    Changes made to nu metro again

--- src/Components/Option/Option.jsx
+++ src/Components/Option/Option.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Option.css'
+import { MdOutlineLocalMovies } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+
 
 
 const Option = () => {
@@ -8,13 +11,15 @@ const Option = () => {
           <hr /><p>OR</p><hr />
       </div><div className="choices">
             <div className="choose">
+            <FaLocationDot />
             <p>Choose a cinema</p>
             </div>
             <div className="choose">
-
+            <MdOutlineLocalMovies />
+            <p>hjb</p>
             </div>
             <div className="choose">
-
+            <p>ycdgbx</p>
             </div>
         </div></>
   )
